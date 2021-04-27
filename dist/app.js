fe7adf78007aa1fadfffaa217ba29128c9054634
Davide Mannarelli <davidemannarelli24@gmail.com>
2021-04-27 14:26:57 +0200

    header + main first section: no vue navbar

--- dist/app.js
+++ dist/app.js
@@ -7,11 +7,56 @@
   \********************/
 /***/ (() => {
 
-function init() {
-  console.log("Hello World");
+function initVue() {
+  new Vue({
+    el: '#app',
+    data: {
+      'jumbotron': ["img/jumbotron-1.jpg", "img/jumbotron-2.jpg", "img/jumbotron-3.jpg"],
+      'jumbotronSlides': [{
+        'title': 'Contemporary Ideas',
+        'text': 'Lorem ipsum',
+        'button': true,
+        'videoplayer': false
+      }, {
+        'title': 'Contemporary Ideas',
+        'text': 'Lorem ipsum',
+        'button': true,
+        'videoplayer': false
+      }, {
+        'title': 'Contemporary Ideas',
+        'text': 'Lorem ipsum',
+        'button': true,
+        'videoplayer': false
+      }],
+      'img': '',
+      'cont': 0
+    },
+    methods: {
+      slider: function slider() {
+        this.img = this.jumbotron[this.cont];
+        return this.img;
+      },
+      getClick: function getClick() {
+        this.cont += 1;
+        console.log(this.cont);
+        this.cont = this.cont >= this.jumbotron.length ? 0 : this.cont = this.cont;
+        return this.cont;
+      },
+      removeClick: function removeClick() {
+        this.cont -= 1;
+        console.log(this.cont);
+        this.cont = this.cont < 0 ? 3 : this.cont = this.cont;
+        return this.cont;
+      }
+    }
+  });
 }
 
-init();
+function init() {
+  initVue();
+}
+
+document.addEventListener('DOMContentLoaded', init);
 
 /***/ }),
 
