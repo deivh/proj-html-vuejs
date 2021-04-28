8f9c9b9400406d2d1081034cb66556e004c425a2
Davide Mannarelli <davidemannarelli24@gmail.com>
2021-04-28 17:31:26 +0200

    header: vue works finally

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,86 @@
   \********************/
 /***/ (() => {
 
+function initVue() {
+  new Vue({
+    el: '#app',
+    data: {
+      'navbarMenus': [{
+        'label': 'menu',
+        'content': 'home'
+      }, {
+        'label': 'menu',
+        'content': 'courses'
+      }, {
+        'label': 'menu',
+        'content': 'instructors'
+      }, {
+        'label': 'menu',
+        'content': 'events'
+      }, {
+        'label': 'menu',
+        'content': 'pages'
+      }, {
+        'label': 'menu',
+        'content': 'elements'
+      }, {
+        'label': 'icon',
+        'content': 'fas fa-search'
+      }, {
+        'label': 'icon',
+        'content': 'fas fa-shopping-bag'
+      }, {
+        'label': 'icon',
+        'content': 'fas fa-bars'
+      }],
+      'jumbotronSlides': [{
+        'title': 'Contemporary Ideas',
+        'text': 'Lorem ipsum',
+        'button': true,
+        'videoplayer': false,
+        'img': "img/jumbotron-1.jpg"
+      }, {
+        'title': 'Contemporary Ideas',
+        'text': 'Lorem ipsum',
+        'button': true,
+        'videoplayer': false,
+        'img': "img/jumbotron-2.jpg"
+      }, {
+        'title': 'Contemporary Ideas',
+        'text': 'Lorem ipsum',
+        'button': true,
+        'videoplayer': false,
+        'img': "img/jumbotron-3.jpg"
+      }],
+      'img': '',
+      'cont': 0
+    },
+    methods: {
+      slider: function slider() {
+        this.img = this.jumbotronSlides[this.cont].img;
+        return this.img;
+      },
+      getClick: function getClick() {
+        this.cont += 1;
+        console.log(this.cont);
+        this.cont = this.cont >= this.jumbotronSlides.length ? 0 : this.cont = this.cont;
+        return this.cont;
+      },
+      removeClick: function removeClick() {
+        this.cont -= 1;
+        console.log(this.cont);
+        this.cont = this.cont < 0 ? 3 : this.cont = this.cont;
+        return this.cont;
+      }
+    }
+  });
+}
 
+function init() {
+  initVue();
+}
+
+document.addEventListener('DOMContentLoaded', init); // $(init);
 
 /***/ }),
 
--- src/app.js
+++ src/app.js
@@ -2,29 +2,65 @@ function initVue() {
     new Vue({
         el: '#app', 
         data: {
-            'images' : 
-                        ["img/jumbotron-1.jpg",
-                         "img/jumbotron-2.jpg",
-                         "img/jumbotron-3.jpg"
-                        ],
+            'navbarMenus' : [
+                {
+                  'label' : 'menu',
+                  'content' : 'home' 
+                },
+                {
+                    'label' : 'menu',
+                    'content' : 'courses' 
+                },
+                {
+                  'label' : 'menu',
+                  'content' : 'instructors' 
+                },
+                {
+                  'label' : 'menu',
+                  'content' : 'events' 
+                },
+                {
+                  'label' : 'menu',
+                  'content' : 'pages' 
+                },
+                {
+                  'label' : 'menu',
+                  'content' : 'elements' 
+                },
+                {
+                  'label' : 'icon',
+                  'content' : 'fas fa-search' 
+                },
+                {
+                  'label' : 'icon',
+                  'content' : 'fas fa-shopping-bag' 
+                },
+                {
+                  'label' : 'icon',
+                  'content' : 'fas fa-bars' 
+                }
+            ],
             'jumbotronSlides': [
                 {
                     'title' : 'Contemporary Ideas',
                     'text'  : 'Lorem ipsum',
                     'button' : true,
                     'videoplayer': false,
+                    'img': "img/jumbotron-1.jpg"
                 },
                 {
                     'title' : 'Contemporary Ideas',
                     'text'  : 'Lorem ipsum',
                     'button' : true,
                     'videoplayer': false,
+                    'img': "img/jumbotron-2.jpg"
                 },
                 {
                     'title' : 'Contemporary Ideas',
                     'text'  : 'Lorem ipsum',
                     'button' : true,
                     'videoplayer': false,
+                    'img': "img/jumbotron-3.jpg"
                 }
             ],
             'img' : '',
@@ -33,13 +69,13 @@ function initVue() {
         },
         methods: {
              slider: function(){
-                 this.img = this.images[this.cont];
+                 this.img = this.jumbotronSlides[this.cont].img;
                  return this.img;
              },
              getClick: function() {
                 this.cont  += 1;
                 console.log(this.cont);
-                this.cont = this.cont >= this.images.length  ? 0 : this.cont = this.cont
+                this.cont = this.cont >= this.jumbotronSlides.length  ? 0 : this.cont = this.cont
                  return this.cont;
              },
              removeClick: function() {
@@ -56,5 +92,5 @@ function init() {
 }
 
 
-document.addEventListener('DOMContentLoaded', init)
+document.addEventListener('DOMContentLoaded', init);
 // $(init);
