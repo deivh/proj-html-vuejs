4c515637c80fec548d057c65d042904ffcca546a
Davide Mannarelli <davidemannarelli24@gmail.com>
2021-04-28 12:16:53 +0200

    main: 5th, 6th, 7th, sections data and layout

--- dist/app.js
+++ dist/app.js
@@ -7,58 +7,7 @@
   \********************/
 /***/ (() => {
 
-function initVue() {
-  new Vue({
-    el: '#app',
-    data: {
-      'images': ["img/jumbotron-1.jpg", "img/jumbotron-2.jpg", "img/jumbotron-3.jpg"],
-      'jumbotronSlides': [{
-        'title': 'Contemporary Ideas',
-        'text': 'Lorem ipsum',
-        'button': true,
-        'videoplayer': false
-      }, {
-        'title': 'Contemporary Ideas',
-        'text': 'Lorem ipsum',
-        'button': true,
-        'videoplayer': false
-      }, {
-        'title': 'Contemporary Ideas',
-        'text': 'Lorem ipsum',
-        'button': true,
-        'videoplayer': false
-      }],
-      'img': '',
-      'cont': 0
-    },
-    methods: {
-      slider: function slider() {
-        this.img = this.images[this.cont];
-        return this.img;
-      },
-      getClick: function getClick() {
-        this.cont += 1;
-        console.log(this.cont);
-        this.cont = this.cont >= this.images.length ? 0 : this.cont = this.cont;
-        return this.cont;
-      },
-      removeClick: function removeClick() {
-        this.cont -= 1;
-        console.log(this.cont);
-        this.cont = this.cont < 0 ? 3 : this.cont = this.cont;
-        return this.cont;
-      }
-    }
-  });
-}
 
-function init() {
-  initVue();
-}
-
-init();
-console.log("hello world");
-document.addEventListener('DOMContentLoaded', init); // $(init);
 
 /***/ }),
 
---
+++ test.js
@@ -0,0 +1,60 @@
+function initVue() {
+    new Vue({
+        el: '#appC', 
+        data: {
+            'images' : 
+                        ["img/jumbotron-1.jpg",
+                         "img/jumbotron-2.jpg",
+                         "img/jumbotron-3.jpg"
+                        ],
+            'jumbotronSlides': [
+                {
+                    'title' : 'Contemporary Ideas',
+                    'text'  : 'Lorem ipsum',
+                    'button' : true,
+                    'videoplayer': false,
+                },
+                {
+                    'title' : 'Contemporary Ideas',
+                    'text'  : 'Lorem ipsum',
+                    'button' : true,
+                    'videoplayer': false,
+                },
+                {
+                    'title' : 'Contemporary Ideas',
+                    'text'  : 'Lorem ipsum',
+                    'button' : true,
+                    'videoplayer': false,
+                }
+            ],
+            'img' : '',
+            'cont' : 0,
+
+        },
+        methods: {
+             slider: function(){
+                 this.img = this.images[this.cont];
+                 return this.img;
+             },
+             getClick: function() {
+                this.cont  += 1;
+                console.log(this.cont);
+                this.cont = this.cont >= this.images.length  ? 0 : this.cont = this.cont
+                 return this.cont;
+             },
+             removeClick: function() {
+                this.cont  -= 1;
+                console.log(this.cont);
+                this.cont = this.cont < 0  ? 3 : this.cont = this.cont
+                 return this.cont;
+             }
+    }});
+}
+
+function init() {
+    initVue();
+}
+
+
+document.addEventListener('DOMContentLoaded', init)
+// $(init);
